8009525c784342fd2688e938b56a0cd3b7355ef1
Enora Fremy <enora.fremy@orange.fr>
2020-05-04 18:13:31 +0200

    mv_PerLinesHisto _PerCondition

---
+++ R/plots_mv_PerLinesHisto.R
@@ -0,0 +1,69 @@
+#' This method plots a bar plot which represents the distribution of the 
+#' number of missing values (NA) per lines (ie proteins).
+#' 
+#' @title Bar plot of missing values per lines using highcharter
+#' @param qData A matrix that contains the data to plot.
+#' @param samplesData A dataframe which contains informations about 
+#' the replicates.
+#' @param indLegend The indice of the column names in \code{colData()}
+#' @return A bar plot
+#' @author Samuel Wieczorek, Enora Fremy
+#' @examples
+#' library(highcharter)
+#' library(DAPAR2)
+#' utils::data(Exp1_R25_pept, package='DAPARdata2')
+#' qData <- assay(Exp1_R25_pept[['original']])
+#' samplesData <- colData(Exp1_R25_pept)
+#' mvPerLinesHisto_HC(qData, samplesData)
+#' @export
+#' @import highcharter
+mvPerLinesHisto_HC <- function(qData, samplesData, indLegend="auto"){
+  
+  samplesData <- as.matrix(samplesData)
+  
+  if (identical(indLegend,"auto")) { indLegend <- c(2:length(colnames(samplesData)))}
+  
+  for (j in 1:length(colnames(qData))){
+    noms <- NULL
+    for (i in 1:length(indLegend)){
+      noms <- paste(noms, samplesData[j,indLegend[i]], sep=" ")
+    }
+    colnames(qData)[j] <- noms
+  }
+  
+  coeffMax <- .1
+  
+  NbNAPerCol <- colSums(is.na(qData))
+  NbNAPerRow <- rowSums(is.na(qData))
+  
+  nb.col <- dim(qData)[2] 
+  nb.na <- NbNAPerRow
+  temp <- table(NbNAPerRow)
+  nb.na2barplot <- c(temp, rep(0,1+ncol(qData)-length(temp)))
+  
+  if (sum(NbNAPerRow) == 0){
+    nb.na2barplot <- rep(0,1+ncol(qData))
+  }
+  
+  df <- data.frame(y=nb.na2barplot[-1])
+  myColors = rep("lightgrey",nrow(df))
+  myColors[nrow(df)] <- "red"
+  
+  
+  
+  h1 <-  highchart() %>% 
+    hc_title(text = "#[lines] with X NA values") %>% 
+    hc_add_series(data = df, type="column", colorByPoint = TRUE) %>%
+    hc_colors(myColors) %>%
+    hc_plotOptions( column = list(stacking = "normal"),
+                    animation=list(duration = 100)) %>%
+    hc_legend(enabled = FALSE) %>%
+    hc_xAxis(categories = row.names(df), title = list(text = "#[NA values] per line")) %>%
+    dapar_hc_ExportMenu(filename = "missingValuesPlot1") %>%
+    hc_tooltip(enabled = TRUE,
+               headerFormat= '',
+               pointFormat = "{point.y} ")
+  
+  return(h1)
+  
+}
---
+++ R/plots_mv_PerLinesHistoPerCondition.R
@@ -0,0 +1,69 @@
+#' This method plots a bar plot which represents the distribution of the 
+#' number of missing values (NA) per lines (ie proteins) and per conditions.
+#' 
+#' @title Bar plot of missing values per lines and per condition
+#' @param qData A dataframe that contains quantitative data.
+#' @param samplesData A dataframe where lines correspond to samples and 
+#' columns to the meta-data for those samples.
+#' @param indLegend The indice of the column names of \code{colData()}
+#' @param palette A vector of HEX Code colors (one color 
+#' per condition).
+#' @return A bar plot
+#' @author Samuel Wieczorek, Enora Fremy
+#' @examples
+#' library(DAPAR)
+#' library(highcharter)
+#' utils::data(Exp1_R25_pept, package='DAPARdata2')
+#' qData <- assay(Exp1_R25_pept[['original']])
+#' samplesData <- colData(Exp1_R25_pept)
+#' mvPerLinesHistoPerCondition_HC(qData, samplesData)
+#' @export
+#' @import highcharter
+mvPerLinesHistoPerCondition_HC <- function(qData, samplesData, indLegend="auto", palette=NULL){
+  
+  conds <- samplesData[,"Condition"]
+  if (is.null(palette)) { palette <- BuildPalette(conds, palette) } 
+  
+  if (identical(indLegend,"auto")) { indLegend <- c(2:length(colnames(samplesData)))}
+  
+  nbConditions <- length(unique(samplesData[,"Condition"]))
+  
+  ncolMatrix <- max(unlist(lapply(unique(samplesData[,"Condition"]), function(x){length(which(samplesData[,"Condition"]==x))})))
+  m <- matrix(rep(0, nbConditions*(1+ncolMatrix)), 
+              ncol = nbConditions, 
+              dimnames=list(seq(0:(ncolMatrix)),unique(samplesData[,"Condition"])))
+  
+  for (i in unique(samplesData[,"Condition"])) {
+    nSample <- length(which(samplesData[,"Condition"] == i))
+    t <- NULL
+    if (nSample == 1) {
+      t <- table(as.integer(is.na(qData[,which(samplesData[,"Condition"] == i)])))
+    } else {t <- table(rowSums(is.na(qData[,which(samplesData[,"Condition"] == i)])))}
+    
+    m[as.integer(names(t))+1,i] <- t
+  }
+  
+  m <- as.data.frame(m)
+  
+  rownames(m) <- 0:(nrow(m)-1)
+  
+  h1 <-  highchart() %>% 
+    hc_title(text = "#[lines] with X NA values (condition-wise)") %>% 
+    dapar_hc_chart(chartType = "column") %>%
+    hc_plotOptions( column = list(stacking = ""),
+                    dataLabels = list(enabled = FALSE),
+                    animation=list(duration = 100)) %>%
+    hc_colors(unique(palette)) %>%
+    hc_legend(enabled = FALSE) %>%
+    hc_xAxis(categories = row.names(m), title = list(text = "#[NA values] per line (condition-wise)")) %>%
+    dapar_hc_ExportMenu(filename = "missingValuesPlot_2") %>%
+    hc_tooltip(headerFormat= '',
+               pointFormat = "{point.y} ")
+  
+  for (i in 1:nbConditions){
+    h1 <- h1 %>% hc_add_series(data=m[,unique(samplesData[,"Condition"])[i]]) }
+  
+  
+  return(h1)
+  
+}
